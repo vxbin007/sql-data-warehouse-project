589b81f741359e4a4ac8a020ceb8917b02ebfb9c
AJ Balazuela <balazuela.alvinjohn@gmail.com>
2025-06-26 12:09:39 +0800

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -2,9 +2,10 @@
 =================================================================================================
 DDL Script	 	: Create Gold Views
 Purpose:		: This script creates views for the Gold layer in the data warehouse. 
-				  The Gold layer represents the final dimension and fact tables (Star Schema)
-				  Each view performs transformations and combines data from the Silver layer 
-				  to produce a clean, enriched, and business-ready dataset.
+
+			 The Gold layer represents the final dimension and fact tables (Star Schema)
+			 Each view performs transformations and combines data from the Silver layer 
+			 to produce a clean, enriched, and business-ready dataset.
 
 Usage			: These views can be queried directly for analytics and reporting.
 =================================================================================================
